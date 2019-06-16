0d9641576e3a8ab3f33dbed26c32707ecc29aef2
salishop <37633640+salishop@users.noreply.github.com>
2019-06-16 20:07:49 +0300

    Updates

---
+++ precache-manifest.90bf401c794548dd534a0151c091a478.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13e90494fa1b2a9605ad58c91c46e4e",
+    "url": "/todo_app/index.html"
+  },
+  {
+    "revision": "858caf713c401567c5e3",
+    "url": "/todo_app/static/css/main.56dd364a.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/todo_app/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "858caf713c401567c5e3",
+    "url": "/todo_app/static/js/main.ff226c93.chunk.js"
+  },
+  {
+    "revision": "6c1ee6e246643c8a5a1d",
+    "url": "/todo_app/static/js/runtime~main.da743e9d.js"
+  }
+]);
